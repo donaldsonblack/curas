a773c0b959a365cb2451288a224e46a77f3f8157
Harrison Black <harrison.black18@Gmail.com>
2025-09-16 15:29:54 +1000

    feat(authz): Added AuthzService with auth booleans

--- src/main/java/dev/donaldsonblack/cura/service/AuthzService.java
+++ src/main/java/dev/donaldsonblack/cura/service/AuthzService.java
@@ -1,22 +1,54 @@
-// package dev.donaldsonblack.cura.service;
-//
-//
-// import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
-// import org.springframework.stereotype.Service;
-//
-// import dev.donaldsonblack.cura.repository.ChecklistRepository;
-// import dev.donaldsonblack.cura.repository.DepartmentRepository;
-// import dev.donaldsonblack.cura.repository.EquipmentRepository;
-// import lombok.AllArgsConstructor;
-//
-// import org.springframework.security.core.Authentication;
-//
-// @Service("authz")
-// @AllArgsConstructor
-// public class AuthzService {
-// 	private final ChecklistRepository checklists;
-// 	private final EquipmentRepository equipment;
-// 	private final DepartmentRepository departments;
-//
-//
-// }
+package dev.donaldsonblack.cura.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
+import org.springframework.stereotype.Service;
+
+import dev.donaldsonblack.cura.model.*;
+import dev.donaldsonblack.cura.repository.*;
+import lombok.AllArgsConstructor;
+
+@Service("authzService")
+@AllArgsConstructor
+public class AuthzService {
+
+	private final UserRepository userRepository;
+	private final EquipmentRepository equipmentRepository;
+	private final UserDepartmentRepository userDepartmentRepository;
+
+	public boolean userInDepartment(Integer deptId, Integer userId) {
+		if (userDepartmentRepository.existsById(new UserDepartmentId(userId, deptId))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean userInDepartmentOfEquipment(Integer userId, Integer equipId) {
+		Equipment equip = equipmentRepository.getReferenceById(equipId);
+		Integer deptId = equip.getDepartmentId();
+
+		if (userInDepartment(deptId, userId)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean userInDepartmentBySub(Integer deptId,UUID sub) {
+		Optional<User> u = userRepository.findBySub(sub);
+
+		if (u.isEmpty()) {
+			return false;
+		}
+		Integer id = u.get().getId();
+
+		if (userInDepartment(deptId, id)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
