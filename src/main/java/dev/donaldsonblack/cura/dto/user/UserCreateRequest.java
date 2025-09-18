ea6e2ca79785d77192375ff91f08f2335dae05f5
Harrison Black <harrison.black18@Gmail.com>
2025-09-18 20:10:52 +1000

    refactor(userEndpoint): Removed unused imports

--- src/main/java/dev/donaldsonblack/cura/dto/user/UserCreateRequest.java
+++ src/main/java/dev/donaldsonblack/cura/dto/user/UserCreateRequest.java
@@ -2,13 +2,10 @@ package dev.donaldsonblack.cura.dto.user;
 
 import java.util.UUID;
 
-
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
 
 public record UserCreateRequest(@NotBlank String fname, @NotBlank String lname, @NotBlank @Email String email,
 		@NotNull UUID sub) {
-}
+} 
