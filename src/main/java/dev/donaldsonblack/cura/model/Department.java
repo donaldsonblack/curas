db2a6f9c51e33a53178e60e76172c1672f395c45
Harrison Black <harrison.black18@Gmail.com>
2025-09-18 20:02:59 +1000

    feat(departmentEndpoint): Updated department class

--- src/main/java/dev/donaldsonblack/cura/model/Department.java
+++ src/main/java/dev/donaldsonblack/cura/model/Department.java
@@ -1,13 +1,10 @@
 package dev.donaldsonblack.cura.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import dev.donaldsonblack.cura.config.JsonViews;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import java.util.LinkedHashSet;
-import java.util.Set;
 import lombok.*;
 
 @Entity
@@ -16,29 +13,24 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Department {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@JsonView(JsonViews.departmentMinimal.class)
-  private Integer id;
+	private Integer id;
 
-  @NotBlank
-  @Column(nullable = false, unique = true)
+	@NotBlank
+	@Column(nullable = false, unique = true)
 	@JsonView(JsonViews.departmentMinimal.class)
-  private String name;
+	private String name;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "parent_id")
-  @JsonIgnore
-  private Department parent;
+	@Column(name = "parent_id")
+	@JsonView(JsonViews.departmentMinimal.class)
+	private Integer parent;
 
-  @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
-  @JsonIgnore
-  private Set<Department> children = new LinkedHashSet<>();
-
-  @Transient
-  public boolean isRoot() {
-    return parent == null;
-  }
+	@Column(name = "location")
+	@JsonView(JsonViews.departmentMinimal.class)
+	private String location;
 }
