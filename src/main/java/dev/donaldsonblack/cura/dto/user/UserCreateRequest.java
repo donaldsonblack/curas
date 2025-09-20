fbbfb43612e55ddc65272df10f2b57c4f0693e35
Harrison Black <harrison.black18@Gmail.com>
2025-09-20 14:13:55 +1000

    feat(authz): Added Roles to user create and update request

--- src/main/java/dev/donaldsonblack/cura/dto/user/UserCreateRequest.java
+++ src/main/java/dev/donaldsonblack/cura/dto/user/UserCreateRequest.java
@@ -5,8 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
+import dev.donaldsonblack.cura.model.Role;
+
 public record UserCreateRequest(
     @NotBlank String fname,
     @NotBlank String lname,
     @NotBlank @Email String email,
-    @NotNull UUID sub) {}
+    @NotNull UUID sub,
+		Role role) {}
--- src/main/java/dev/donaldsonblack/cura/dto/user/UserUpdateRequest.java
+++ src/main/java/dev/donaldsonblack/cura/dto/user/UserUpdateRequest.java
@@ -3,5 +3,7 @@ package dev.donaldsonblack.cura.dto.user;
 import java.util.Optional;
 import java.util.UUID;
 
+import dev.donaldsonblack.cura.model.Role;
+
 public record UserUpdateRequest(
-    Optional<String> fname, Optional<String> lname, Optional<String> email, Optional<UUID> sub) {}
+    Optional<String> fname, Optional<String> lname, Optional<String> email, Optional<UUID> sub, Optional<Role> role) {}
