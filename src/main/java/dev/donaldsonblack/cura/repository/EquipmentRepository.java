80d54ac6a8324df5b49980758c1e95c38038b46f
Harrison Black <harrison.black18@Gmail.com>
2025-09-20 14:16:03 +1000

    feat(authz): Update Equipment repository with queries

--- src/main/java/dev/donaldsonblack/cura/repository/EquipmentRepository.java
+++ src/main/java/dev/donaldsonblack/cura/repository/EquipmentRepository.java
@@ -2,5 +2,9 @@ package dev.donaldsonblack.cura.repository;
 
 import dev.donaldsonblack.cura.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {}
+public interface EquipmentRepository extends JpaRepository<Equipment, Integer> {
+	@Query(value = "select e.department.id from Equipment e where e.id=:id", nativeQuery = true)
+	Integer findDeptId(Integer id);
+}
