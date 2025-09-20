70b8f02eafc5271b3a728e76399de13de4259ece
Harrison Black <harrison.black18@Gmail.com>
2025-09-20 14:15:04 +1000

    feat(authz): Added department queries into service layer

--- src/main/java/dev/donaldsonblack/cura/service/DepartmentService.java
+++ src/main/java/dev/donaldsonblack/cura/service/DepartmentService.java
@@ -5,6 +5,8 @@ import dev.donaldsonblack.cura.dto.department.DepartmentUpdateRequest;
 import dev.donaldsonblack.cura.model.Department;
 import dev.donaldsonblack.cura.repository.DepartmentRepository;
 import java.util.Optional;
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,48 +16,52 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DepartmentService {
 
-  private final DepartmentRepository repo;
+	private final DepartmentRepository repo;
 
-  public Page<Department> list(Pageable pageable) {
-    return repo.findAll(pageable);
-  }
+	public Page<Department> list(Pageable pageable) {
+		return repo.findAll(pageable);
+	}
 
-  public Optional<Department> getById(Integer id) {
-    return repo.findById(id);
-  }
+	public Optional<Department> getById(Integer id) {
+		return repo.findById(id);
+	}
 
-  public void delete(Integer id) {
-    repo.deleteById(id);
-  }
+	public void delete(Integer id) {
+		repo.deleteById(id);
+	}
 
-  public Department save(DepartmentCreateRequest req) {
-    Department d =
-        Department.builder().name(req.name()).location(req.location()).parent(req.parent()).build();
+	public Department save(DepartmentCreateRequest req) {
+		Department d = Department.builder().name(req.name()).location(req.location()).parent(req.parent()).build();
 
-    return repo.save(d);
-  }
+		return repo.save(d);
+	}
 
-  public Optional<Department> update(Integer id, DepartmentUpdateRequest req) {
-    return repo.findById(id)
-        .map(
-            dep -> {
-              req.name().ifPresent(dep::setName);
-              req.location().ifPresent(dep::setLocation);
-              req.parent().ifPresent(dep::setParent);
+	public Optional<Department> update(Integer id, DepartmentUpdateRequest req) {
+		return repo.findById(id)
+				.map(
+						dep -> {
+							req.name().ifPresent(dep::setName);
+							req.location().ifPresent(dep::setLocation);
+							req.parent().ifPresent(dep::setParent);
 
-              return repo.save(dep);
-            });
-  }
+							return repo.save(dep);
+						});
+	}
 
-  public Optional<Department> put(Integer id, DepartmentCreateRequest req) {
-    return repo.findById(id)
-        .map(
-            dep -> {
-              dep.setName(req.name());
-              dep.setLocation(req.location());
-              dep.setParent(req.parent());
+	public Optional<Department> put(Integer id, DepartmentCreateRequest req) {
+		return repo.findById(id)
+				.map(
+						dep -> {
+							dep.setName(req.name());
+							dep.setLocation(req.location());
+							dep.setParent(req.parent());
 
-              return repo.save(dep);
-            });
-  }
+							return repo.save(dep);
+						});
+	}
+
+	public Page<Department> findAllForUser(UUID sub, Pageable pageable) {
+		return repo.findAllForUser(sub, pageable);
+	}
+
 }
