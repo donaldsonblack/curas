ecca663fe70ce573ec75eb65a350598bd7efcd1d
Harrison Black <harrison.black18@Gmail.com>
2025-09-20 14:14:45 +1000

    feat(authz): Added more DepartmentRepository queries

--- src/main/java/dev/donaldsonblack/cura/repository/DepartmentRepository.java
+++ src/main/java/dev/donaldsonblack/cura/repository/DepartmentRepository.java
@@ -3,12 +3,23 @@ package dev.donaldsonblack.cura.repository;
 import dev.donaldsonblack.cura.model.Department;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-  Optional<Department> findByName(String name);
+	Optional<Department> findByName(String name);
 
-  List<Department> findByParentIsNull();
+	List<Department> findByParentIsNull();
 
-  List<Department> findByParent(Integer parent);
+	List<Department> findByParent(Integer parent);
+
+	@Query("select d from Department d " +
+			"join UserDepartment ud on ud.department.id = d.id " +
+			"where ud.user.sub = :sub")
+	Page<Department> findAllForUser(@Param("sub") UUID sub, Pageable pageable);
 }
