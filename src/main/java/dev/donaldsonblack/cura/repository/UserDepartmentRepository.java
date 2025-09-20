e629a42d2b661d6a6541c13bf246011c95f32737
Harrison Black <harrison.black18@Gmail.com>
2025-09-20 14:15:22 +1000

    feat(authz): Added userDepartmentRepo queries

--- src/main/java/dev/donaldsonblack/cura/repository/UserDepartmentRepository.java
+++ src/main/java/dev/donaldsonblack/cura/repository/UserDepartmentRepository.java
@@ -3,18 +3,36 @@ package dev.donaldsonblack.cura.repository;
 import dev.donaldsonblack.cura.model.UserDepartment;
 import dev.donaldsonblack.cura.model.UserDepartmentId;
 import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDepartmentRepository extends JpaRepository<UserDepartment, UserDepartmentId> {
-  @EntityGraph(attributePaths = {"department"})
-  List<UserDepartment> findAllByUserId(Integer userId);
+	@EntityGraph(attributePaths = { "department" })
+	List<UserDepartment> findAllByUserId(Integer userId);
 
-  // @EntityGraph(attributePaths = { "user" })
-  // Page<UserDepartment> findAllByDepartmentId(
-  // 		Integer departmentId,
-  // 		Pageable pageable);
+	List<UserDepartment> findAllByUserSub(UUID sub);
 
-  @EntityGraph(attributePaths = {"user"})
-  List<UserDepartment> findAllByDepartmentId(Integer deptId);
+	// @EntityGraph(attributePaths = { "user" })
+	// Page<UserDepartment> findAllByDepartmentId(
+	// Integer departmentId,
+	// Pageable pageable);
+
+	@EntityGraph(attributePaths = { "user" })
+	List<UserDepartment> findAllByDepartmentId(Integer deptId);
+
+	@Query("select (count(ud) > 0) " +
+			"from UserDepartment ud " +
+			"where ud.user.sub = :sub and ud.department.id = :deptId")
+	boolean isMember(@Param("sub") String sub, @Param("deptId") Integer deptId);
+
+	@Query("select (count(ud) > 0) " +
+			"from UserDepartment ud " +
+			"where ud.user.sub = :sub and ud.department.id = :deptId " +
+			"and (ud.role = dev.donaldsonblack.cura.model.Role.MANAGER " +
+			"     or ud.role = dev.donaldsonblack.cura.model.Role.ADMIN)")
+	boolean isManager(@Param("sub") String sub, @Param("deptId") Integer deptId);
 }
