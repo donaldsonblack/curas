20db86dd0f842f5231d5198f3416aebca8cb9de8
Harrison Black <harrison.black18@Gmail.com>
2025-09-20 14:16:13 +1000

    feat(authz): Update user repository with Queries

--- src/main/java/dev/donaldsonblack/cura/repository/UserRepository.java
+++ src/main/java/dev/donaldsonblack/cura/repository/UserRepository.java
@@ -4,9 +4,18 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import dev.donaldsonblack.cura.model.Role;
 import dev.donaldsonblack.cura.model.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public Optional<User> findBySub(UUID sub);
+	public Optional<User> findBySub(UUID sub);
+
+	@Query("select u.role from User u where u.sub = :sub")
+	Optional<Role> findRoleBySub(@Param("sub") UUID sub);
+
+	@Query("select u.id from User u where u.sub = :sub")
+	Optional<Integer> findIdBySub(@Param("sub") UUID sub);
 }
