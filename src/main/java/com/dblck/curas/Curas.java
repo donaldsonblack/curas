b61b23396517467df863f0db5ecedb68d367101b
Harrison Black <harrison.black18@Gmail.com>
2025-10-15 23:15:44 +1100

    feat(cache): Enable caching

--- src/main/java/com/dblck/curas/Curas.java
+++ src/main/java/com/dblck/curas/Curas.java
@@ -2,12 +2,14 @@ package com.dblck.curas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableCaching
 @SpringBootApplication
 public class Curas {
-  public static void main(String[] args) {
-    SpringApplication.run(Curas.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(Curas.class, args);
+	}
 }
